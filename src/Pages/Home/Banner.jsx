17ed68087181f1b26c5946e03a25dbc8d59c6484
tsakib2000 <isakib49@gmail.com>
2025-01-20 17:29:46 +0600

    fixed banner text

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,60 +1,25 @@
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+
 import slide1 from "../../assets/slide-1.jpg";
-import slide2 from "../../assets/slide-2.jpg";
-import slide3 from "../../assets/slide-3.jpg";
-import slide4 from "../../assets/slide-4.jpg";
-import slide5 from "../../assets/slide-5.jpg";
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+
 const Banner = () => {
   return (
-    <div className="mb-5">
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-        }}
-      
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className="h-[450px] w-full">
-            <img className="w-full h-full object-cover" src={slide1} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="h-[450px] w-full">
-            <img className="w-full h-full object-cover" src={slide2} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="h-[450px] w-full">
-            <img className="w-full h-full object-cover" src={slide3} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="h-[450px] w-full">
-            <img className="w-full h-full object-cover" src={slide4} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="h-[450px] w-full">
-            <img className="w-full h-full object-cover" src={slide5} alt="" />
-          </div>
-        </SwiperSlide>
-      </Swiper>
+<div
+  className="hero h-[500px]"
+  style={{
+    backgroundImage: `url(${slide1})`,
+  }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Empowering Collaboration</h1>
+      <p className="mb-5">
+    Discover the power of teamwork and shared learning. Join us to innovate, inspire, and achieve together in a collaborative environment designed for success.
+      </p>
+ 
     </div>
+  </div>
+</div>
   );
 };
 
