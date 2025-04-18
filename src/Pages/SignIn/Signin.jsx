f107fd5d188f83e78fd619ccb0ea2c2743057081
tsakib2000 <isakib49@gmail.com>
2025-04-18 22:42:36 +0600

    added admin log in button

--- src/Pages/SignIn/Signin.jsx
+++ src/Pages/SignIn/Signin.jsx
@@ -9,91 +9,96 @@ import { useState } from "react";
 
 const Signin = () => {
 
-    const {signInUser}=useAuth();
-const navigate=useNavigate();
-const [credential,setCredential]=useState()
+  const { signInUser } = useAuth();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-    const handleSignin =async e=>{
-        e.preventDefault();
-        const form=e.target;
-        const email=form.email.value;
-        const password = form.password.value;
-        try{
-            await  signInUser(email,password)
-            navigate('/')
-            toast.success('Signin successful')
-        }catch(err){
-            toast.error(err.message)
-        }
-    
+  const handleSignin = async e => {
+    e.preventDefault();
+
+    try {
+      await signInUser(email, password)
+      navigate('/')
+      toast.success('Signin successful')
+    } catch (err) {
+      toast.error(err.message)
     }
-    return (
-        <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content grid grid-cols-12 ">
-         
-            
 
-          <div className="card bg-base-100 col-span-12 md:col-span-7 w-full   shadow-2xl">
+  }
+  const handleCredential = () => {
+    setEmail('isakib49@gmail.com')
+    setPassword("123456")
+  }
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content grid grid-cols-12 ">
 
-            <form onSubmit={handleSignin} className="card-body">
+
+
+        <div className="card bg-base-100 col-span-12 md:col-span-7 w-full   shadow-2xl">
+
+          <form onSubmit={handleSignin} className="card-body">
             <h1 className="text-2xl text-center font-bold">Login now</h1>
-        <button className="btn bg-[#58a6af] text-white">Admin</button>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  name="email"
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  name="password"
-                  type="password"
-                  placeholder="password"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn bg-[#58a6af] text-white rounded-none">
-                  Signin
-                </button>
-              </div>
-              <div className="divider">OR</div>
-              <GoogleLogin/>
-              
-              <GithubLogin/>
+            <button onClick={handleCredential} type="button" className="btn bg-[#58a6af] text-white">Admin</button>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                defaultValue={email}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                defaultValue={password}
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn bg-[#58a6af] text-white rounded-none">
+                Signin
+              </button>
+            </div>
+            <div className="divider">OR</div>
+            <GoogleLogin />
 
-              <div className="flex items-center justify-between mt-4">
-            <span className="w-1/5 border-b  md:w-1/4"></span>
+            <GithubLogin />
 
-            <Link
-              to="/signup"
-              className="text-xs text-gray-500 uppercase  hover:underline"
-            >
-              or sign up
-            </Link>
+            <div className="flex items-center justify-between mt-4">
+              <span className="w-1/5 border-b  md:w-1/4"></span>
 
-            <span className="w-1/5 border-b  md:w-1/4"></span>
-          </div>
-            </form>
-         
-          </div>
-                   <div className="hidden md:block  md:col-span-5">
-<Lottie animationData={login}></Lottie>
-         </div>
-          
+              <Link
+                to="/signup"
+                className="text-xs text-gray-500 uppercase  hover:underline"
+              >
+                or sign up
+              </Link>
+
+              <span className="w-1/5 border-b  md:w-1/4"></span>
+            </div>
+          </form>
+
         </div>
+        <div className="hidden md:block  md:col-span-5">
+          <Lottie animationData={login}></Lottie>
+        </div>
+
       </div>
-    );
+    </div>
+  );
 };
 
 export default Signin;
